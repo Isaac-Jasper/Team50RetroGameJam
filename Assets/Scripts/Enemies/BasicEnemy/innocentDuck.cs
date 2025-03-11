b4f6845eef52ef32894098d69d10fec1ddb300fb
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2025-03-11 14:31:48 -0700

    enemy movement implemented
    basic enemy movement works. gets a little wonky at high speeds and can get stuck in corners sometimes. To fix this I suggest 'bumpers' in the corner that send duck toward middle.

--- Assets/Scripts/Enemies/BasicEnemy/innocentDuck.cs
+++ Assets/Scripts/Enemies/BasicEnemy/innocentDuck.cs
@@ -6,24 +6,21 @@ public class InnocentDuck : Enemy
     [SerializeField]
     private Rigidbody2D rb;
 
-    [Range(0.0f, 50.0f)]
+    [Header("% of circle path can deviate")]
+    [Range(0.0f, 0.25f)]
     [SerializeField]
-    private float angleRange;
-    private Vector3 moveDirection;
-    private bool inArena;
-
-    private Vector2 hackyVelocity;
+    private float angleRange; //deviates on both sides of path, for a total of 50% circle coverage
+    private Vector3 moveDirection; //direction duck is currently moving
+    private Vector2 nonZeroVelocity; //velocity of duck right before colliding with wall
 
     override protected void Start() {
         base.Start();
         OnSpawn();
     }
     void Update() {
-        if (rb.linearVelocity.magnitude >= enemyMoveSpeed) {
-            hackyVelocity = rb.linearVelocity;
+        if (rb.linearVelocity.magnitude > 0) {
+            nonZeroVelocity = rb.linearVelocity;
         }
-        //moveDirection = new Vector2(Mathf.Cos(2*Mathf.PI*moveAngle), Mathf.Sin(2*Mathf.PI*moveAngle));
-        //OnMove();
     }
     protected override void OnAim() {
         //no aim
@@ -44,67 +41,63 @@ public class InnocentDuck : Enemy
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        if (!inArena) return;
-
+        //collision is a bit wonky at high speeds. I suspect what happens is the duck clips into the wall
+        //and collides once and then collides again on the way back giving a wierd negative bounce that sticks
+        //it to the wall.
         if (col.collider.CompareTag("East Wall")) {
             //find angle after bounce
-            float bounceAngle = Mathf.Atan2(hackyVelocity.y, -hackyVelocity.x)/(2*Mathf.PI);
+            float bounceAngle = Mathf.Atan2(nonZeroVelocity.y, -nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
             
-            Debug.Log(bounceAngle + " y = " + hackyVelocity.y + " x = " + (-hackyVelocity.x));
+            //Debug.Log(bounceAngle + " y = " + hackyVelocity.y + " x = " + (-hackyVelocity.x));
 
-            //max min currently breaking. need to have the value be able to wrap from 1 to 0
-            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange/100, 0.25f), Mathf.Min(bounceAngle+angleRange/100, 0.75f));
-            Debug.Log("New angle:" + newAngle);
+            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange, 0.25f), Mathf.Min(bounceAngle+angleRange, 0.75f));
+            //Debug.Log("New angle:" + newAngle);
             //0.25 to 0.75
             moveDirection = new Vector2(Mathf.Cos(2*Mathf.PI*newAngle), Mathf.Sin(2*Mathf.PI*newAngle));
             OnMove();
             return;
         }
-        if (col.collider.CompareTag("West Wall")) {//find angle after bounce
-            float bounceAngle = Mathf.Atan2(hackyVelocity.y, -hackyVelocity.x)/(2*Mathf.PI);
+        if (col.collider.CompareTag("West Wall")) {
+            //find angle after bounce
+            float bounceAngle = Mathf.Atan2(nonZeroVelocity.y, -nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
-            Debug.Log(bounceAngle + " y = " + hackyVelocity.y + " x = " + (-hackyVelocity.x));
+            //Debug.Log(bounceAngle + " y = " + hackyVelocity.y + " x = " + (-hackyVelocity.x));
 
-            //max min currently breaking. need to have the value be able to wrap from 1 to 0
-            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange/100, -0.25f), Mathf.Min(bounceAngle+angleRange/100, 0.25f));
-            Debug.Log("New angle:" + newAngle);
+            //avoids dealing with the 1 not wrapping to 0
+            float temp = bounceAngle + 0.25f;
+            if (temp > 1) temp--;
+            float newAngle = Random.Range(Mathf.Max(temp-angleRange, 0f), Mathf.Min(temp+angleRange, 0.5f));
+            newAngle -= 0.25f;
+            //Debug.Log("New angle:" + newAngle);
             //-0.25 to 0.25
             moveDirection = new Vector2(Mathf.Cos(2*Mathf.PI*newAngle), Mathf.Sin(2*Mathf.PI*newAngle));
             OnMove();
             return;
         }
         if (col.collider.CompareTag("North Wall")) {
-            float bounceAngle = Mathf.Atan2(-hackyVelocity.y, hackyVelocity.x)/(2*Mathf.PI);
-            if (bounceAngle < 0) bounceAngle++;
-            Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
+            float bounceAngle = Mathf.Atan2(-nonZeroVelocity.y, nonZeroVelocity.x)/(2*Mathf.PI);
+            if (bounceAngle < 0) bounceAngle++; 
+            //Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
 
-            //max min currently breaking. need to have the value be able to wrap from 1 to 0
-            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange/100, 0.5f), Mathf.Min(bounceAngle+angleRange/100, 1));
-            Debug.Log("New angle:" + newAngle);
+            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange, 0.5f), Mathf.Min(bounceAngle+angleRange, 1));
+            //Debug.Log("New angle:" + newAngle);
             //0.5 to 1
             moveDirection = new Vector2(Mathf.Cos(2*Mathf.PI*newAngle), Mathf.Sin(2*Mathf.PI*newAngle));
             OnMove();
             return;
         }
         if (col.collider.CompareTag("South Wall")) {
-            float bounceAngle = Mathf.Atan2(-hackyVelocity.y, hackyVelocity.x)/(2*Mathf.PI);
+            float bounceAngle = Mathf.Atan2(-nonZeroVelocity.y, nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
-            Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
+            //Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
 
-            //max min currently breaking. need to have the value be able to wrap from 1 to 0
-            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange/100, 0), Mathf.Min(bounceAngle+angleRange/100, 0.5f));
-            Debug.Log("New angle:" + newAngle);
+            float newAngle = Random.Range(Mathf.Max(bounceAngle-angleRange, 0), Mathf.Min(bounceAngle+angleRange, 0.5f));
+            //Debug.Log("New angle:" + newAngle);
             moveDirection = new Vector2(Mathf.Cos(2*Mathf.PI*newAngle), Mathf.Sin(2*Mathf.PI*newAngle));
             //0 to 0.5
             OnMove();
             return;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D col) {
-        if (col.CompareTag("Arena")) {
-            inArena = true;
         }
     }
 }
