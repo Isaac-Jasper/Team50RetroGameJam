53479d05040df1d8f5006a5620e4cdba0b4ec110
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2025-03-18 08:38:16 -0700

    movement now works at high speeds

--- Assets/Scripts/Enemies/BasicEnemy/innocentDuck.cs
+++ Assets/Scripts/Enemies/BasicEnemy/innocentDuck.cs
@@ -41,10 +41,12 @@ public class InnocentDuck : Enemy
     }
 
     void OnCollisionEnter2D(Collision2D col) {
-        //collision is a bit wonky at high speeds. I suspect what happens is the duck clips into the wall
-        //and collides once and then collides again on the way back giving a wierd negative bounce that sticks
-        //it to the wall.
         if (col.collider.CompareTag("East Wall")) {
+            if (rb.linearVelocity.x < 0) return;
+            if (rb.linearVelocity.x == 0) {
+                moveDirection = Vector2.left;
+                OnMove();
+            }
             //find angle after bounce
             float bounceAngle = Mathf.Atan2(nonZeroVelocity.y, -nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
@@ -59,6 +61,11 @@ public class InnocentDuck : Enemy
             return;
         }
         if (col.collider.CompareTag("West Wall")) {
+            if (rb.linearVelocity.x > 0) return;
+            if (rb.linearVelocity.x == 0) {
+                moveDirection = Vector2.right;
+                OnMove();
+            }
             //find angle after bounce
             float bounceAngle = Mathf.Atan2(nonZeroVelocity.y, -nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
@@ -76,6 +83,11 @@ public class InnocentDuck : Enemy
             return;
         }
         if (col.collider.CompareTag("North Wall")) {
+            if (rb.linearVelocity.y < 0) return;
+            if (rb.linearVelocity.y == 0) {
+                moveDirection = Vector2.down;
+                OnMove();
+            }
             float bounceAngle = Mathf.Atan2(-nonZeroVelocity.y, nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++; 
             //Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
@@ -88,6 +100,11 @@ public class InnocentDuck : Enemy
             return;
         }
         if (col.collider.CompareTag("South Wall")) {
+            if (rb.linearVelocity.y > 0) return;
+            if (rb.linearVelocity.y == 0) {
+                moveDirection = Vector2.up;
+                OnMove();
+            }
             float bounceAngle = Mathf.Atan2(-nonZeroVelocity.y, nonZeroVelocity.x)/(2*Mathf.PI);
             if (bounceAngle < 0) bounceAngle++;
             //Debug.Log(bounceAngle + " y = " + (-hackyVelocity.y) + " x = " + hackyVelocity.x);
