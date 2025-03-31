b3b0f8bac82f9d3c0b86aa7f022ab96805b9fd6a
hatllama <zakipughfradot@gmail.com>
2025-03-30 23:32:58 -0700

    Made infinite scene

--- Assets/Scripts/RoundManager/RoundController.cs
+++ Assets/Scripts/RoundManager/RoundController.cs
@@ -21,30 +21,25 @@ public class RoundController : MonoBehaviour
 
     public void StartRound()
     {
-        for(int i = 0; i < enemies.Length; i++)
-        {
-            EnemyBase temp = enemies[i];
-            int r = Random.Range(i, enemies.Length);
-            enemies[i] = enemies[r];
-            enemies[r] = temp;
-        }
-
         enemiesSpawned = 0;
         enemiesAlive = 0;
         StartCoroutine(SpawnTimer());
     }
 
     private IEnumerator SpawnTimer()
+{
+    while (true) 
     {
-        while (enemiesSpawned < enemiesPerRound)
+        int enemiesToSpawn = Mathf.Min(5, enemies.Length); 
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
-            if (enemiesPerBurst > enemiesPerRound - enemiesSpawned) enemiesPerBurst = enemiesPerRound - enemiesSpawned;
-            for (int i = 0; i < enemiesPerBurst; i++)
-                SpawnEnemy(enemies[enemiesSpawned], spawnLocations[Random.Range(0, spawnLocations.Length)].position);
-
-            yield return new WaitForSeconds(timeBetweenEnemies);
+            SpawnEnemy(enemies[Random.Range(0, enemies.Length)], 
+                      spawnLocations[Random.Range(0, spawnLocations.Length)].position);
         }
+
+        yield return new WaitForSeconds(timeBetweenEnemies); 
     }
+}
 
     private void SpawnEnemy(EnemyBase enemy, Vector2 position)
     {
